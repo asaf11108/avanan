c28344369bcc9379d9e804809f1be7db58a28912
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-05-24 18:02:30 +0300

    addded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchFieldComponent } from './components/search-field/search-field.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFieldComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
